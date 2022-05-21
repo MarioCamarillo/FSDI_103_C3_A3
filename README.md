5364cbe34836d1a80a4343760df3ae27c8afbed7
MarioCamarillo <104475782+MarioCamarillo@users.noreply.github.com>
2022-05-21 14:06:46 -0700

    FSDI_103_C3_A3
    This is the calculator assignment.

---
+++ README.md
@@ -0,0 +1,9 @@
+# 103 Class 3 Assignment 3
+
+The caluculator
+
+This is the assignment 3 of class 3.
+
+Create a javascript program that asks the user for two numbers. These numbers will interact with each other and produce math operations: summation, substraction, multiplication and division.
+
+Placed individual id for each operation instead of using the same instruction with paragraph separation
---
+++ calculator.js
@@ -0,0 +1,50 @@
+
+
+function calculation(){
+    let num1 = Number(prompt("Enter the first number "));
+    let num2 = Number(prompt("Enter the last number"));
+    
+    document.getElementById("sum").innerHTML=`${num1} + ${num2} = ${sum(num1,num2)}`;
+
+    document.getElementById("substraction").innerHTML=`${num1} - ${num2} = ${substraction(num1,num2)}`;
+
+    document.getElementById("multiplication").innerHTML=`${num1} * ${num2} = ${multiplication(num1,num2)}`;
+
+    document.getElementById("division").innerHTML=`${num1} / ${num2} = ${division(num1,num2)}`;
+
+
+}
+
+////////////////////////////////
+// Sum operation
+////////////////////////////////
+function sum(a,b){
+    return a+b;
+
+}
+
+
+
+////////////////////////////////
+// Sub operation
+////////////////////////////////
+function substraction(a,b){
+    return a-b;
+
+}
+
+////////////////////////////////
+// Multi operation
+////////////////////////////////
+function multiplication(a,b){
+    return a*b;
+
+}
+
+////////////////////////////////
+// Div operation
+////////////////////////////////
+function division(a,b){
+    return a/b;
+
+}
